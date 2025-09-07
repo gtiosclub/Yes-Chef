35974c570072ef1c9220a0eea56e467703c3cd76
Krish Prasad <krishkp00@gmail.com>
2025-09-07 15:02:55 -0400

    add folder structure

---
+++ Yes Chef/Models/Firebase.swift
@@ -0,0 +1,14 @@
+//
+//  Firebase.swift
+//  Yes Chef
+//
+//  Created by Krish Prasad on 9/7/25.
+//
+
+import Foundation
+import Firebase
+
+class Firebase {
+    static let db = Firestore.firestore()
+}
+
---
+++ Yes Chef/Remixing/test.swift
@@ -0,0 +1,7 @@
+//
+//  test.swift
+//  Yes Chef
+//
+//  Created by Krish Prasad on 9/7/25.
+//
+
