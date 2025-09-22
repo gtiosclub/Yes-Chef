6268b3265d5a99b43674a7851101bdaccaf56676
NityaPotti <nityapotti01@gmail.com>
2025-09-22 10:38:30 -0400

    OpenAI API Key

---
+++ Yes Chef/Common/ViewModels/AIViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  AIViewModel.swift
+//  Yes Chef
+//
+//  Created by Nitya Potti on 9/22/25.
+//
+
+import Foundation
+import FirebaseFirestore
+
+class AIViewModel: ObservableObject {
+    @Published var openaiKey: String?
+    let db = Firestore.firestore()
+    private func fetchAPIKey() async {
+        do {
+            let document = try await db.collection("APIKEYS").document("OpenAI").getDocument()
+            if let data = document.data(), let key = data["key"] as? String {
+                DispatchQueue.main.async {
+                    self.openaiKey = key
+                }
+            } else {
+                print("No key found in document")
+            }
+        } catch {
+            print("Error fetching API key from Firestore: \(error)")
+        }
+    }
+}
