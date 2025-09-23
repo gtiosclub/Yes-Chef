a728d4a93ec7a1419fb093e99f8b3c5af9bc9b98
nkrishna42 <nkrishna42@gatech.edu>
2025-09-23 14:39:40 -0400

    History tab, but has erorr with firebase

---
+++ Yes Chef/Remixing/HistoryViewModel.swift
@@ -0,0 +1,32 @@
+//
+//  HistoryViewModel.swift
+//  Yes Chef
+//
+//  Created by Nidhi Krishna on 9/22/25.
+//
+
+import FirebaseFirestore
+import SwiftUI
+
+class HistoryViewModel {
+    @Published var history: [HistoryBlock] = []
+    private var db = Firestore.firestore()
+
+    
+    func fetchHistory() {
+        db.collection("leaderboardHistory").getDocuments { snapshot, error in
+                if let error = error {
+                    print("Error: \(error)")
+                    return
+                }
+                
+            self.history = snapshot?.documents.compactMap { doc in
+                let data = doc.data()
+                let title = data["Title"] as? String ?? "Unknown"
+                let challengeName = data["challengeName"] as? String ?? "Unknown"
+                            
+                return HistoryBlock(id: doc.documentID, title: title, challengeName: challengeName)
+            } ?? []
+        }
+    }
+}
