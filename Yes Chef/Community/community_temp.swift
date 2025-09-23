9b68c876a1d57781db03b04aa2b3b8d1a114a1e8
Will Patton <willapatton@icloud.com>
2025-09-23 17:14:20 -0400

    Update community_temp.swift
    Created a basic user settings view to change password, username, and delete account. The button actions are not programmed, only visually.

--- Yes Chef/Community/community_temp.swift
+++ Yes Chef/Community/community_temp.swift
@@ -5,3 +5,60 @@
 //  Created by Krish Prasad on 9/7/25.
 //
 
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                Text("Settings")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding(.top, 40)
+                
+                // Change Password Button
+                Button(action: {
+                    // Code to change password
+                }) {
+                    Text("Change Password")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+
+                // Change Username Button
+                Button(action: {
+                    // Code to change username
+                }) {
+                    Text("Change Username")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+
+                // Delete Account Button
+                Button(action: {
+                    // Code to delete account
+                }) {
+                    Text("Delete Account")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
