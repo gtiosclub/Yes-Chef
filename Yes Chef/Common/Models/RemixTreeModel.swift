3a6ee51d7c34dfb4f46e36d59ae85ac82682982b
nikola-cao <cao.nikola.a@gmail.com>
2025-09-30 15:52:18 -0400

    some node model changes, created remix tree model, added some helper functions for deleting, adding, and finding

--- Yes Chef/Common/Models/RemixTreeModel.swift
+++ Yes Chef/Common/Models/RemixTreeModel.swift
@@ -9,26 +9,72 @@ import Foundation
 class RemixTreeNode  {
     
     let currNodeID: String
-    let parentNodeID: String?
-    let rootNodeOfTreeID: String
+    var parentNode: RemixTreeNode?
+    let rootNodeOfTree: RemixTreeNode
     
-    var childrenIDs: [String]
+    var children: [RemixTreeNode]
     
     var descriptionOfRecipeChanges: String
     
     init(currNodeID: String,
-         parentNodeID: String?,
-         rootNodeOfTreeID: String,
-         childrenIDs: [String],
+         parentNode: RemixTreeNode?,
+         rootNodeOfTree: RemixTreeNode,
+         children: [RemixTreeNode],
          descriptionOfRecipeChanges: String = "") {
+        
         self.currNodeID = currNodeID
-        self.parentNodeID = parentNodeID
-        self.rootNodeOfTreeID = rootNodeOfTreeID
-        self.childrenIDs = childrenIDs
+        self.parentNode = parentNode
+        self.rootNodeOfTree = rootNodeOfTree
+        self.children = children
         self.descriptionOfRecipeChanges = descriptionOfRecipeChanges
     }
 }
 
 class RemixTreeModel {
     
+    let rootNode: RemixTreeNode
+    
+    init(rootNode: RemixTreeNode) {
+        self.rootNode = rootNode
+    }
+    
+    /**
+    Should delete a node from the remix tree. If a node is deleted and it has children, the parent of the children get reassigned to the parent of the deleted node.
+     */
+    func deleteNode(node: RemixTreeNode) {
+        
+        
+    }
+    
+    func addNode(nodeID: String, parentNode: RemixTreeNode?, rootNodeOfTree: RemixTreeNode, children: [RemixTreeNode], descriptionOfRecipeChanges: String = "") {
+        
+        let newNode = RemixTreeNode(currNodeID: nodeID,
+                                    parentNode: parentNode,
+                                    rootNodeOfTree: rootNodeOfTree,
+                                    children: children,
+                                    descriptionOfRecipeChanges: descriptionOfRecipeChanges)
+        parentNode?.children.append(newNode)
+    }
+    
+    func findNode(nodeID: String) -> RemixTreeNode? {
+        
+        return findNodeHelper(currNode: rootNode, destNodeID: nodeID)
+    }
+    
+    private func findNodeHelper(currNode: RemixTreeNode, destNodeID: String) -> RemixTreeNode? {
+        
+        if currNode.currNodeID == destNodeID {
+            return currNode
+        }
+        
+        
+        for childNode in currNode.children {
+            
+            if let found = findNodeHelper(currNode: childNode, destNodeID: destNodeID) {
+                return found
+            }
+        }
+        
+        return nil
+    }
 }
