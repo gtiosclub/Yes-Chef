c43d3c2f83263f7a7bac7f93f41f063a83f5ae47
Kushi Kashyap <kushikash06@gmail.com>
2025-10-09 17:53:36 -0400

    implemented search functionality to find users in community view and ability to click on username to access profile

--- Yes Chef/Common/Views/Home.swift
+++ Yes Chef/Common/Views/Home.swift
@@ -10,37 +10,48 @@ import SwiftUI
 
 struct Home: View {
     @State var selectedView: TabSelection = .home
+    @State var authViewModel = AuthenticationVM()
     var body: some View {
-        
-        TabView(selection: $selectedView) {
-            FeedView().tabItem {
-                Image(systemName: "house")
-            }.tag(TabSelection.home)
-            CommunityView().tabItem {
-                Image(systemName: "magnifyingglass")
-            }.tag(TabSelection.search)
-<<<<<<< Updated upstream
-            CreateRecipe().tabItem {
-=======
-            CommunityView().tabItem {
->>>>>>> Stashed changes
-                Image(systemName: "plus.circle")
-            }.tag(TabSelection.post)
-            LeaderboardView().tabItem {
-                Image(systemName: "trophy")
-            }.tag(TabSelection.leaderboard)
-            ProfileView(isOwnProfile: true).tabItem {
-                Image(systemName: "person.circle")
-            }.tag(TabSelection.profile)
+        NavigationStack {
+            TabView(selection: $selectedView) {
+                FeedView().tabItem {
+                    Image(systemName: "house")
+                }.tag(TabSelection.home)
+                CommunityView().tabItem {
+                    Image(systemName: "magnifyingglass")
+                }.tag(TabSelection.search)
+                CreateRecipe().tabItem {
+                    Image(systemName: "plus.circle")
+                }.tag(TabSelection.post)
+                LeaderboardView().tabItem {
+                    Image(systemName: "trophy")
+                }.tag(TabSelection.leaderboard)
+                if let currentUser = authViewModel.currentUser {
+                    ProfileView(user: currentUser, isOwnProfile: true)
+                        .tabItem {
+                            Image(systemName: "person.circle")
+                        }
+                        .tag(TabSelection.profile)
+                } else {
+                    ProgressView()
+                        .tabItem {
+                            Image(systemName: "person.circle")
+                        }
+                        .tag(TabSelection.profile)
+                    //                ProfileView(isOwnProfile: true).tabItem {
+                    //                    Image(systemName: "person.circle")
+                    //                }.tag(TabSelection.profile)
+                }
+            }
         }
     }
-}
-
-
-enum TabSelection {
-    case home, search, post, leaderboard, profile
-}
-
-#Preview {
-    Home()
+    
+    
+    enum TabSelection {
+        case home, search, post, leaderboard, profile
+    }
+    
+//    #Preview {
+//        Home()
+//    }
 }
