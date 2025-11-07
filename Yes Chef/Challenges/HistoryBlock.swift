44e6f7b6b9901d5b710a544a6298167ae92149d9
Eesh Majithia <emajithia3@gmail.com>
2025-11-07 01:29:16 -0500

    Refactor HistoryBlock to use Firebase document ID

--- Yes Chef/Challenges/HistoryBlock.swift
+++ Yes Chef/Challenges/HistoryBlock.swift
@@ -5,10 +5,21 @@
 //  Created by Nidhi Krishna on 9/20/25.
 //
 import SwiftUI
+import FirebaseFirestore
 
 struct HistoryBlock: Identifiable, Equatable {
-    let id = UUID()
+    let id: String // Firebase document ID
     var date: String
     var challengeName: String
-    
+    var submissions: [String] // Array of recipe IDs
+    var archivedAt: Date?
+
+    // Convenience initializer for backward compatibility
+    init(id: String = UUID().uuidString, date: String, challengeName: String, submissions: [String] = [], archivedAt: Date? = nil) {
+        self.id = id
+        self.date = date
+        self.challengeName = challengeName
+        self.submissions = submissions
+        self.archivedAt = archivedAt
+    }
 }
